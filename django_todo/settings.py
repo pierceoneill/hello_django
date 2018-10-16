4f7075acd66655e223538ccb74f482a2077e78c5
Pierce ONeill <pierceoneillky@gmail.com>
2018-10-16 16:31:29 +0000

    update ALLOWED HOSTS

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '8-*9n$*uf#n=(^@s^62me1z7j4xvb*h9v$xe+gp^4iv4p-f0c$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-todo-pierceoneill.c9users.io']
+ALLOWED_HOSTS = ['django-todo-pierceoneill.c9users.io',
+                'simple-django-todo-pon.herokuapp.com']
 
 
 # Application definition
